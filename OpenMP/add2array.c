361d1a962392d6d041cebb6b79a699f2d5e65c70
Pranamya Jain <146780116+pranamyajainn@users.noreply.github.com>
2025-01-16 21:53:39 +0530

    Update add2array.c

--- OpenMP/add2array.c
+++ OpenMP/add2array.c
@@ -2,17 +2,17 @@
 #include <omp.h>
 
 int main() {
-    int n = 5; // Size of the arrays
-    int a[5] = {1, 2, 3, 4, 5}; // First array
-    int b[5] = {5, 4, 3, 2, 1}; // Second array
-    int c[5]; // Resultant array
+    int n = 5;
+    int a[5] = {1, 2, 3, 4, 5}; 
+    int b[5] = {5, 4, 3, 2, 1}; 
+    int c[5]; 
 
-    int chunk_size = 2; // Define chunk size for dynamic scheduling
+    int chunk_size = 2; 
 
     // Perform parallel addition
     #pragma omp parallel for schedule(dynamic, chunk_size)
     for (int i = 0; i < n; i++) {
-        c[i] = a[i] + b[i]; // Add corresponding elements
+        c[i] = a[i] + b[i]; 
         printf("Thread %d added index %d: %d + %d = %d\n",
                omp_get_thread_num(), i, a[i], b[i], c[i]);
     }
